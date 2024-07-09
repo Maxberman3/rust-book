9fe3169e12c5d9bd5f4acb6b3700ff2d2d72a4e3
max-buywith <max@buywith.com>
2024-07-09 09:50:22 +0300

    add adder crate/testing chapter

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,24 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}.");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -22,9 +40,11 @@ fn main() {
             Err(_) => continue,
         };
 
+        let guess_struct = Guess::new(guess as i32);
+
         println!("You guessed: {guess}");
 
-        match guess.cmp(&secret_number) {
+        match guess_struct.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
@@ -32,5 +52,16 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
