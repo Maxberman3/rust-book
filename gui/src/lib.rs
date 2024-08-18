88234e1bb93254e3a17e0a03bfa3376ed0239fe1
maxberman <max@getleo.ai>
2024-08-18 21:55:53 +0300

    add new gui crate

---
+++ gui/src/lib.rs
@@ -0,0 +1,71 @@
+// main advantage of trait objects vs generic types with trait bounds is you can substitute multiple different types that implement the trait
+// as opposed to generic, that can be anything that implements until one is picked.
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+// IN CALLING CODE
+// use gui::{Button, Screen};
+
+// fn main() {
+//     let screen = Screen {
+//         components: vec![
+//             Box::new(SelectBox {
+//                 width: 75,
+//                 height: 10,
+//                 options: vec![
+//                     String::from("Yes"),
+//                     String::from("Maybe"),
+//                     String::from("No"),
+//                 ],
+//             }),
+//             Box::new(Button {
+//                 width: 50,
+//                 height: 10,
+//                 label: String::from("OK"),
+//             }),
+//         ],
+//     };
+
+//     screen.run();
+// }
+
+// trait objects require dynamic dispatch which occurs at runtime, as opposed to static dispatch which occurs at compile time and is used by
+// generics. So this extra flexibility comes with a runtime cost.
